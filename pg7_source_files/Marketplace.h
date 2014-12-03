6cfd3b4ddc77bc423769c56dd6ab9e52555cd52d
emirosejohn <emilyjohnson84@ymail.com>
2014-12-03 17:28:01 -0500

    Changed Marketplace.h
    added:
     isValid
    constructor
    deconstructor
    addCard
    replaceCard

--- pg7_source_files/Marketplace.h
+++ pg7_source_files/Marketplace.h
@@ -10,17 +10,25 @@
 #define pg7_Marketplace_h
 
 #include "Container.h"
+#include "Game.h"
+#include "Cards.h"
+#include <vector>
+#include <iostream>
+
+using std::vector;
 
 class Marketplace: public Container{
 protected:
     vector<Card> market;
     
 public:
-    Marketplace();
-    virtual ~Marketplace(){market.clear};
+    Marketplace(class Game a);
+    bool isValid(){if(market.size()==5) return true; else return false;};
+    virtual ~Marketplace(){market.clear();};
     Card getCard(int place)const{return market.at(place);} ;  //place is spot in vector. first place ==0
-    std::string* getMarket();
-    void addCard(Card c)const {market.push_back(c);};
+    //std::string* getMarket();
+    void addCard(Card c) {market.push_back(c);};
+    void replaceCard(int Ind, Card c){ market.at(Ind)=c;};
 };
 
 #endif
